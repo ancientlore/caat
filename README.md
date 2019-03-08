7a3c44094cd086066d4a19b97a3d233506cba93c
Michael Lore <mike.lore@sap.com>
2019-03-08 14:30:03 -0500

    Also does gti

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 Command caat prints out a picture of a cat on high-color terminals. It's
 useful when you accidentally type caat instead of cat.
 
+You can also `ln -s caat gti` and it will print a GTI.
+
 
 
 
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 /*
 Command caat prints out a picture of a cat on high-color terminals. It's
 useful when you accidentally type caat instead of cat.
+
+You can also `ln -s caat gti` and it will print a GTI.
 */
 package main
 
-//go:generate binder -o cat.go cat.txt
+//go:generate binder -o cat.go cat.txt gti.txt
 
 // Use github.com/davecheney/godoc2md to generate README file.
 //go:generate bash -c "godoc2md -ex . | sed -e 's/\\/src\\/target\\///g' -e 's/import \".\"/import \"github.com\\/ancientlore\\/caat\"/' > README.md"
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println(string(Lookup("/cat.txt")))
+	var img string
+	exe, err := os.Executable()
+	if err == nil {
+		exe = filepath.Base(exe)
+		switch exe {
+		case "gti":
+			img = "/gti.txt"
+		default:
+			img = "/cat.txt"
+		}
+		fmt.Println(exe)
+	}
+	fmt.Println(string(Lookup(img)))
 }
